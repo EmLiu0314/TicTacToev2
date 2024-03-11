868346635da2bdce63e785d04fe883fff30c1bab
EmLiu0314 <emilyliu52106@gmail.com>
2024-03-10 23:41:13 -0500

    Initial Commit

---
+++ TicTacToev1/Game Screen/GameSquare.swift
@@ -0,0 +1,36 @@
+//
+//  GameSquare.swift
+//  TicTacToev1
+//
+//  Created by Rajiv Mukherjee on 3/4/24.
+//
+
+import SwiftUI
+
+struct GameSquare{ //information we want on each tile
+    var id:Int //whiich tile 1 to 9
+    var player: Player? //? means value can be nil
+    //can't modify inside of a structure unless use @state
+    
+    var image:Image{
+        //the line below is not checking for equality.
+        //it is only testing for the existence of value within an optional
+        
+        if let player = player {
+            return player.gamePiece.image
+        }
+        else {
+            return Image("none")
+        }
+    }
+    
+    static var reset:[GameSquare]{ //reset is a function in game square
+        var squares = [GameSquare]() //creating an instance of the GameSquare class
+            for index in 1...9 {
+                squares.append(GameSquare(id: index))
+            }
+            return squares
+            
+        
+    }
+}
